e68d8a92231b46679ca5c2c2b22b244227f42249
dtom777 <ethan230204@gmail.com>
2022-05-10 09:00:10 +0900

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
-const path = require("path");
+const path = require('path');
 // cssがindex.htmlに埋め込まれないようなplugin
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // distのindex.htmlを編集しないで済むようなplugin
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // distの中身を最初にきれいにするplugin(不要なファイルを削除する)
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/javascript/main.js",
+  entry: './src/javascript/main.js',
   output: {
     // path.resolve 絶対パス取得
     // __dirname 現在のディレクトリ
-    path: path.resolve(__dirname, "./dist"),
-    filename: "./javascript/main.js", // defaultでmain.js
+    path: path.resolve(__dirname, './dist'),
+    filename: './javascript/main.js', // defaultでmain.js
   },
   module: {
     rules: [
+      //ES6のトランスパイル
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: '> 0.25%, not dead' }],
+              ],
+            },
+          },
+        ],
+      },
       {
         test: /\.css/,
         use: [
@@ -24,7 +39,7 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
         ],
       },
@@ -32,10 +47,10 @@ module.exports = {
         test: /\.(png|jpg)/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
               esModule: false,
-              name: "images/[name].[ext]",
+              name: 'images/[name].[ext]',
             },
           },
         ],
@@ -44,10 +59,10 @@ module.exports = {
         test: /\.pug/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
           },
           {
-            loader: "pug-html-loader",
+            loader: 'pug-html-loader',
             options: {
               pretty: true,
             },
@@ -58,20 +73,20 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "./stylesheet/main.css",
+      filename: './stylesheet/main.css',
     }),
     // index.htmlにコンテンツを追加する方法
     new HtmlWebpackPlugin({
-      template: "./src/template/index.pug",
-      filename: "index.html",
+      template: './src/template/index.pug',
+      filename: 'index.html',
     }),
     new HtmlWebpackPlugin({
-      template: "./src/template/access.pug",
-      filename: "access.html",
+      template: './src/template/access.pug',
+      filename: 'access.html',
     }),
     new HtmlWebpackPlugin({
-      template: "./src/template/members/taro.pug",
-      filename: "members/taro.html",
+      template: './src/template/members/taro.pug',
+      filename: 'members/taro.html',
     }),
     new CleanWebpackPlugin(),
   ],
